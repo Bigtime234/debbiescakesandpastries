226e229080260db0c02dd62d2a111aa843633bad
Nathaniel Johnson <johnsonnathaniel119@gmail.com>
2025-07-18 04:19:41 -0700

    work rn

--- app/api/uploadthing/upload.ts
+++ app/api/uploadthing/upload.ts
@@ -3,8 +3,8 @@ import {
   generateUploadDropzone,
 } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+import { generateReactHelpers } from "@uploadthing/react"
 
-// Removed unused import: generateReactHelpers
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
